b9380bca5b672d3e833b2a01a43f3181b11dd44b
ViyagulaLincy99 <97622111+ViyagulaLincy99@users.noreply.github.com>
2022-01-15 13:22:32 +0530

    Add files via upload

---
+++ Multplication.java
@@ -0,0 +1,21 @@
+package Looping;
+
+import java.util.Scanner;
+
+public class Multplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=2,i
+		while(num<=10)
+		{
+		for(i=1;i<=10;i++)
+		{
+			System.out.println(i+"*"+num+"="+(num*i));
+		}
+		num++;
+		}
+	
+	}
+
+}
